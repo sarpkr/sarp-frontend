ff222450e66f084afd4a515e6670e080f2b11ba2
dotdotgod <vgb3766@gmail.com>
2022-06-30 07:14:50 +0900

    fix: event listener for tronWeb event
    Signed-off-by: dotdotgod <vgb3766@gmail.com>

--- src/hooks/useGetWalletInfo.ts
+++ src/hooks/useGetWalletInfo.ts
@@ -11,40 +11,45 @@ const useGetWalletInfo = () => {
     isMainNet: true,
   });
 
-  // const handler = useCallback(async (e: MessageEvent<any>) => {
-  //   let address: string | undefined = undefined;
-  //   let isMainNet: boolean = true;
+  const handler = useCallback(async (e: MessageEvent<any>) => {
+    let address: string | undefined = undefined;
+    let isMainNet: boolean = true;
+    if (
+      e.data.message &&
+      (e.data.message.action === "setAccount" ||
+        e.data.message.action === "setNode")
+    ) {
+      if (e.data.message.action === "setNode") {
+        address = window.tronWeb.defaultAddress.base58 || undefined;
+        isMainNet =
+          getIsMainNet(e.data.message.data?.connectNode?.fullNode) ||
+          getIsMainNet(e.data.message.data?.node?.fullNode);
+      }
 
-  //   if (e.data.message && e.data.message.action === "setNode") {
-  //     address = window.tronWeb.defaultAddress.base58 || undefined;
-  //     isMainNet =
-  //       getIsMainNet(e.data.message.data?.connectNode?.fullNode) ||
-  //       getIsMainNet(e.data.message.data?.node?.fullNode);
-  //   }
+      if (e.data.message.action === "setAccount") {
+        address = e.data.message.data?.address || undefined;
+        isMainNet = getIsMainNet(window.tronWeb.fullNode.host);
+      }
 
-  //   if (e.data.message && e.data.message.action === "setAccount") {
-  //     address = e.data.message.data?.address || undefined;
-  //     isMainNet = getIsMainNet(window.tronWeb.fullNode.host);
-  //   }
+      try {
+        const balance = await window.tronWeb.trx.getBalance();
+        setWalletInfo((prev) => ({ ...prev, address, isMainNet, balance }));
+      } catch (error) {
+        setWalletInfo((prev) => ({ ...prev, address, isMainNet }));
+        console.log("handler error on useGetWalletInfo", error);
+      }
+    }
+  }, []);
 
-  //   try {
-  //     const balance = await window.tronWeb.trx.getBalance();
-  //     setWalletInfo((prev) => ({ ...prev, address, isMainNet, balance }));
-  //   } catch (error) {
-  //     setWalletInfo((prev) => ({ ...prev, address, isMainNet }));
-  //     console.log("handler error on useGetWalletInfo", error);
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (window) {
+      window.addEventListener("message", handler);
+    }
 
-  // useEffect(() => {
-  //   if (window) {
-  //     window.addEventListener("message", handler);
-  //   }
-
-  //   return () => {
-  //     window.removeEventListener("message", handler);
-  //   };
-  // }, [handler]);
+    return () => {
+      window.removeEventListener("message", handler);
+    };
+  }, [handler]);
 
   const initWalletInfo = useCallback(async () => {
     if (window.tronWeb && window.tronWeb.defaultAddress.base58) {
